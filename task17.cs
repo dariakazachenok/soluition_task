df50d45a6a9308b58e8f3a9040216160b7d5c5b7
dariakazachenok <kazachenokd@mail.ru>
2018-01-07 21:40:44 +0300

    nasledovanie, virt metod

---
+++ task17.cs
@@ -0,0 +1,64 @@
+﻿
+namespace task17
+{
+    public class Geometricfigure
+    {
+        private int coordScenter; // поля
+       
+        public int CoordScenter { get; set; } //свойство
+
+        public Geometricfigure(int coordScente) //конструктор
+        {
+            CoordScenter = coordScente;
+        }
+    }
+
+    class Sqaure : Geometricfigure // класс наследник 1
+    {
+        public double  SmeStor1 { get; set; } //  знач. смещения стороны
+        public double SmeStor2 { get; set; } //  знач. смещения стороны
+        public double SmeStor3 { get; set; } //  знач. смещения стороны
+        public double SmeStor4 { get; set; } //  знач. смещения стороны
+
+        public Sqaure(int coordScente, double smeStor1, double smeStor2, double smeStor3, double smeStor4) : base(coordScente)
+        {
+            SmeStor1 = smeStor1;
+            SmeStor1 = smeStor2;
+            SmeStor1 = smeStor3;
+            SmeStor1 = smeStor4;
+
+        }
+        public void draw1()
+        {
+            //используя координату центра и значение смещения от неё четырех других сторон "строим" квадрат
+        }
+
+    }
+
+    class Circle : Geometricfigure // класс наследник 2
+    {
+        public double Radius { get; set; } // радиус
+
+        public Circle(int coordScente, double radius) : base(coordScente)
+        {
+            Radius = radius;
+        }
+
+        public void draw2()
+        {
+            //используя центр и радиус "чертим" круг
+        }
+    }
+
+    class task17
+    {
+        static void Main(string[] args)
+        {
+            Sqaure S1 = new Sqaure(6, 12, 12, 12, 12); // создаем объект квадрат и задаем координаты центра и радиус
+            S1.draw1(); // "рисуем" круг
+            Circle C1 = new Circle(6, 10);
+            // создаем объект круг и задаем координаты задаем координаты центра и радиус
+            C1.draw2(); // "рисуем" треугольник
+        }
+    }
+}
---
+++ task20.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task20
+{
+    class Voin
+    {
+         public string Name { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public int Damage { get; set; }
+
+        public Voin(string name)
+        {
+            Name = name;
+            Health = 100;
+            Armor = 0;
+            }
+
+        public virtual  int Geturon(int Damage)
+        {
+            Console.Write(Name + " Войн без брони получит урона:");
+            return Health - Damage;
+        }
+    }
+
+    class VoinEasy : Voin // класс наследник 1
+    {
+        public int Armor { get; set; } //  броня
+
+        public VoinEasy(string name) : base(name)
+        {
+            Armor = 50;
+        }
+
+        public override int Geturon(int Damage)
+        {
+            Console.Write(Name + " Bойн со слабой бронёй получит урон:");
+            return base.Geturon(Damage) + Armor;
+        }
+    }
+
+    class VoinHard : Voin // класс наследник 2
+    {
+        public int Armor { get; set; } //  броня
+        
+        public VoinHard(string name) : base(name)
+        {
+           Armor = 80;
+        }
+
+        public override int Geturon(int Damage)
+        {
+            Console.Write(Name + " Bойн с сильной бронёй получит урон:" );
+            return base.Geturon(Damage) + Armor;
+            }
+    }
+    
+    class task20
+    {
+        static void Main(string[] args)
+        {
+            List<Voin> voins = new List<Voin>();
+            voins.Add(new Voin("Василий"));
+            voins.Add(new VoinEasy("Эдуард"));
+            voins.Add(new VoinHard("Петр"));
+            
+            foreach (Voin p in voins)
+            Console.Write(p.Geturon(50) + "\n");
+            Console.ReadKey();
+        }
+    }
+}
+
+
