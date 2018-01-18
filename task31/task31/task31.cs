7d33f23491338b14dccd58085f2ae2fd5a3dad24
Dasha Shuhlia <kazachenokd@mail.ru>
2018-01-18 22:55:42 +0300

    Value Null

---
+++ task31/task31/task31.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace task31
+{
+
+    public class People
+    {
+        string Name { get; set; }
+        int? Amount { get; set; }
+
+        public People(string name, int? amount)
+        {
+            Name = name;
+            Amount = amount;
+        }
+
+          public void Kids()
+    
+          {
+            string kids;
+            if (Amount == null)
+            {
+                kids = "unknown";
+            }
+            else if (Amount == 0)
+            {
+                kids = "no children";
+            }
+            else
+            {
+                kids = Amount.ToString();
+            }
+            Console.WriteLine(Name + "\n Amount of children -" + kids + "\n");
+            Console.ReadLine();
+          }
+    }
+
+    class task31
+    {
+        static void Main(string[] args)
+        {
+            People people1 = new People("Alex", 4);
+            People people2 = new People("Kate", null);
+            People people3 = new People("Stella", 0);
+            people1.Kids();
+            people2.Kids();
+            people3.Kids();
+        }   
+    }
+}
