86bc8079f0f0118525dba90a686226cab32bcb24
Dasha Shuhlia <kazachenokd@mail.ru>
2018-01-21 20:57:41 +0300

    work with files

---
+++ task11.1/task11.1/Task11.1.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO;
+
+namespace Task11_1
+{
+
+    public class Task11_1
+
+        {
+
+        static void Main(string[] args)
+
+        {
+
+
+            FileStream file1 = new FileStream("D:\\numbers.txt", FileMode.Create); //создаем файловый поток
+            StreamWriter writer = new StreamWriter(file1); //создаем «потоковый писатель» и связываем его с файловым потоком 
+
+
+            for (int i = 0; i < 501; i++)
+            {
+                writer.Write(i + ","); //записываем в файл
+            }
+
+            writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется 
+        }
+    }
+}
---
+++ task11.2/task11.2/Task11_2.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Task11_2
+{
+
+    public class Task11_2
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            string[] arr = { "red", "green", "black", "white", "blue" };
+
+            FileStream colors = new FileStream("D:\\test.txt", FileMode.Create); //создаем файловый поток
+            StreamWriter writer = new StreamWriter(colors); //создаем «потоковый писатель» и связываем его с файловым потоком 
+
+
+            foreach (string color in arr)
+            {
+                writer.WriteLine(color);
+            }
+            writer.Close();
+
+
+            writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется 
+
+
+        }
+    }
+}
